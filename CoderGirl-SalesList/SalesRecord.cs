4ce3ed6c866897ef9f1a34ef9a929cd9e8439c0c
Inna Davis <idavis@cordelllaw.com>
2019-04-03 18:12:53 -0500

    sdfsdf

---
+++ CoderGirl-SalesList/SalesRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoderGirl_SalesList
+{
+    public class SalesRecord
+    {
+        //Region,Country,Item Type,Sales Channel,Order Priority,Order Date
+        public string Region { get; set; }
+
+        public string Country { get; set; }
+        public string ItemType { get; set; }
+        public string SalesChannel { get; set; }
+        public string OrderPriority { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
